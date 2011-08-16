22f6e136e174325aaec3330cd863b426606972b9
Paul Yoder <paulyoder@gmail.com>
2011-08-16 09:06:42 -0500

    using $ in place of jQuery

--- src/jquery.formatNumberInput.js
+++ src/jquery.formatNumberInput.js
@@ -8,7 +8,7 @@
     validKeyCode: function(keyCode, currentValue) {
       if (maxLength > 0 && currentValue.replace(/[^0-9]/g,'').length >= maxLength) { return false; }
       if (keyCode == negativeKey && !allowNegative) { return false; }
-      else { return (jQuery.inArray(keyCode, validKeys) >= 0); }
+      else { return ($.inArray(keyCode, validKeys) >= 0); }
     },
     formatWithCommas: function(number) {
       //scrub number
