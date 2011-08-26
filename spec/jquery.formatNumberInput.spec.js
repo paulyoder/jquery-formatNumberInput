a5c825f1a308794da5eccbb0c702fcacfd25636e
Paul Yoder <paulyoder@gmail.com>
2011-08-26 13:09:12 -0500

    added decimalPlaces option

--- spec/jquery.formatNumberInput.spec.js
+++ spec/jquery.formatNumberInput.spec.js
@@ -37,28 +37,6 @@ describe('jquery.formatNumberInput', function() {
   });
 
   describe('internal methods', function() {
-    describe('invalidKeyCode', function() {
-      describe('maxLength', function() {
-        it('when maxLength is not pass in, it allows large numbers', function() {
-          expect(invalidKeyCode('8', '382,973,829,728,292,832,928')).toBeFalsy();
-        });
-        it('when maxLength = 3 and currentValue.length = 3, invalidKeyCode should return false', function() {
-          expect(invalidKeyCode('8', '333', {'maxLength': 3})).toBeTruthy();
-        });
-        it('when maxLength = 5 and currentValue = "1,234", invalidKeyCode should return true', function() {
-          expect(invalidKeyCode('8', '1,234', {'maxLength': 5})).toBeFalsy();
-        });
-        it('when maxLength = 5 and currentValue = "-1,234", invalidKeyCode should return true', function() {
-          expect(invalidKeyCode('8', '-1,234', {'maxLength': 5})).toBeFalsy();
-        });
-      });
-      describe('allowNegative is false', function() {
-        it('when at max length, it still allows a negative', function() {
-          expect(invalidKeyCode('-', '123', {'allowNegative': true, 'maxLength': 3})).toBeFalsy();
-        });
-      });
-    });
-    
     describe('formatWithCommas', function() {
       it('when less than 3 digits, it returns original value', function() {
         expect(formatWithCommas('123')).toEqual('123');
@@ -99,8 +77,8 @@ describe('jquery.formatNumberInput', function() {
       it('when letters in value, it removes the letters', function() {
         expect(formatWithCommas('2ko392')).toEqual('2,392');
       });
-      it('when maxLength = 8 and more than 8 digits, it truncates down to the last 8 digits', function() {
-        expect(formatWithCommas('-1234568901234', {'maxLength': 8})).toEqual('-68,901,234');
+      it('when maxLength = 8 and more than 8 digits, it truncates down to the first 8 digits', function() {
+        expect(formatWithCommas('-12345678901234', {'maxLength': 8})).toEqual('-12,345,678');
       });
       it('when allowNegative = false, then it deletes negative', function() {
         expect(formatWithCommas('-123', {'allowNegative': false})).toEqual('123');
@@ -115,13 +93,70 @@ describe('jquery.formatNumberInput', function() {
       it('removes leading 0s on negative number', function() {
         expect(formatWithCommas('-001234')).toEqual('-1,234');
       });
+      describe('multiple decimals', function() {
+        it('deletes multiple decimals', function() {
+          expect(formatWithCommas('123.4.', {'decimalPlaces': 2})).toEqual('123.4');
+        });
+        it('deletes multiple decimals on negative number', function() {
+          expect(formatWithCommas('-123.4.', {'decimalPlaces': 2})).toEqual('-123.4');
+        });
+      });
+      describe('decimalPlaces', function() {
+        it('when set to 1, keeps 1 decimal place', function() {
+          expect(formatWithCommas('123.45', {'decimalPlaces': 1})).toEqual('123.4');
+        });
+        it('when set to 2, keeps 2 decimal place', function() {
+          expect(formatWithCommas('123.45928', {'decimalPlaces': 2})).toEqual('123.45');
+        });
+        it('when set to 2, keeps 1 decimal place', function() {
+          expect(formatWithCommas('123.4', {'decimalPlaces': 2})).toEqual('123.4');
+        });
+        it('when set to 2, and no decimals', function() {
+          expect(formatWithCommas('123', {'decimalPlaces': 2})).toEqual('123');
+        });
+        it('when set to 2, and period', function() {
+          expect(formatWithCommas('123.', {'decimalPlaces': 2})).toEqual('123.');
+        });
+      });
+      describe('maxLength', function() {
+        it('when no maxLength, it allows large numbers', function() {
+          expect(formatWithCommas('382,973,829,728,292,832,928')).toEqual('382,973,829,728,292,832,928');
+        });
+        it('when maxLength = 3 it should trim number larger than 3 digits', function() {
+          expect(formatWithCommas('12345', {'maxLength': 3})).toEqual('123');
+        });
+        it('when maxLength = 3 it should not trim number less than 3', function() {
+          expect(formatWithCommas('12', {'maxLength': 3})).toEqual('12');
+        });
+        describe('allowNegative is true', function() {
+          it('should not count the negative sign', function() {
+            expect(formatWithCommas('-123', {'maxLength': 3})).toEqual('-123');
+          });
+        });
+        describe('decimalPlace > 0', function() {
+          it('when maxLength = 3, decimalPlaces = 2', function() {
+            expect(formatWithCommas('123.5',{'maxLength': 3,'decimalPlaces': 2})).toEqual('123.5');
+          });
+          it('when maxLength =3, decimalPlaces = 2 and there are 4 digits and 2 decimal places', function() {
+            expect(formatWithCommas('1234.56',{'maxLength': 3,'decimalPlaces': 2})).toEqual('123.56');
+          });
+          it('when maxLength =3, decimalPlaces = 2 and there are 4 digits and just a period', function() {
+            expect(formatWithCommas('1234.',{'maxLength': 3,'decimalPlaces': 2})).toEqual('123.');
+          });
+        });
+      });
     });
   });
 
   describe('options', function() {
     describe('allowNegative', function() {
-      it('allowNegative is defaulted to true', function() {
-        expect(invalidKeyCode('-', '')).toBeFalsy();
+      it('is defaulted to true', function() {
+        expect(formatWithCommas('-')).toEqual('-');
+      });
+    });
+    describe('decimalPlaces', function() {
+      it('is defaulted to 0', function() {
+        expect(formatWithCommas('123.45')).toEqual('123');
       });
     });
   });
--- src/jquery.formatNumberInput.js
+++ src/jquery.formatNumberInput.js
@@ -2,26 +2,31 @@
   var negativeKey = 45;
   var allowNegative;
   var maxLength;
+  var decimalPlaces;
 
   var methods = {
-    invalidKeyCode: function(keyCode, currentValue) {
-      return (maxLength > 0 &&
-              currentValue.replace(/[^0-9]/g,'').length >= maxLength &&
-              keyCode != negativeKey);
-    },
     formatWithCommas: function(number) {
       //scrub number
-      number = number.replace(/[^-0-9]/g,'')           //delete non negative and non numbers
-                     .replace(/--+/g,'-')              //delete multiple negatives
-                     .replace(/(\d)-/g,'$1')           //delete negatives in middle of number
-                     .replace(/^(-?)0+(\w)/g,'$1$2');  //delete leading zeros but allow single 0
+      number = number.replace(/[^-\.0-9]/g,'')            //only keep numbers, negative and decimal
+                     .replace(/--+/g,'-')                 //delete multiple negatives
+                     .replace(/^(-?\d*\.\d*)\..*$/g,'$1') //only allow one decimal
+                     .replace(/(\d)-/g,'$1')              //delete negatives in middle of number
+                     .replace(/^(-?)0+(\w)/g,'$1$2');     //delete leading zeros but allow single 0
 
       if (maxLength > 0) { 
         //make sure there are not more numbers than the maxLength
-        var regEx = new RegExp("\\d+(\\d{" + maxLength + "}$)", 'g');
-        number = number.replace(regEx,'$1'); 
+        var regEx = new RegExp("^(-?\\d{" + maxLength + "})\\d*(\\.\\d*)?$", 'g');
+        number = number.replace(regEx,'$1$2'); 
       }
+
       if (!allowNegative) { number = number.replace(/-/g,''); }
+
+      if (decimalPlaces == 0) { number = number.replace(/\.\d+$/g,''); }
+      else { 
+        var regEx = new RegExp("(\\.\\d{" + decimalPlaces + "})\\d+$", 'g');
+        number = number.replace(regEx,'$1');
+      }
+
       //add commas before returning
       return number.replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,'); 
     }
@@ -30,22 +35,19 @@
   $.fn.formatNumberInput = function(options) {
     var settings = { 
       'allowNegative': true,
-       'maxLength': 0
+      'maxLength': 0,
+      'decimalPlaces' : 0
     };
     $.extend(settings, options);
     allowNegative = settings['allowNegative'];
     maxLength = settings['maxLength'];
+    decimalPlaces = settings['decimalPlaces'];
     if (settings['returnMethods'] == true) { return methods; }
     
-    return this.keypress(function(e) {
-      //have to reset variables since they went out of scope
+    return this.keyup(function() {
       allowNegative = settings['allowNegative'];
       maxLength = settings['maxLength'];
-      var currentValue = $(this).val();
-      if (methods['invalidKeyCode'](e.which, currentValue)) {
-        e.preventDefault();
-      }
-    }).keyup(function() {
+      decimalPlaces = settings['decimalPlaces'];
       var origNumber = $(this).val();
       var newNumber = methods['formatWithCommas'](origNumber);
       if (newNumber != origNumber) { $(this).val(newNumber); }
